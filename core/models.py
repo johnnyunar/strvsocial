14b3398c49095c5921f0f512e5e3e00714c5fe1c
johnnyunar <johnunar@gmail.com>
2025-03-04 19:21:41 +0100

    Add base model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(_("UUID"), default=uuid.uuid4, editable=False, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
