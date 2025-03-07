d7f6486de42069ed96ce8f4f3b5a724e080c73c6
johnnyunar <johnunar@gmail.com>
2025-03-07 17:47:23 +0100

    Update ContentPost list fields

--- core/admin.py
+++ core/admin.py
@@ -6,7 +6,7 @@ from core.models import ContentPost
 
 @admin.register(ContentPost)
 class ContentPostAdmin(ModelAdmin):
-    list_display = ("title", "created_at", "updated_at")
+    list_display = ("title", "media_type", "user", "created_at", "updated_at")
     search_fields = ("title",)
     date_hierarchy = "created_at"
     list_filter = ("created_at", "updated_at")
